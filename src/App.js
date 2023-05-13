6730e9c61c4910709c52777cb38bc6d7c3d1b458
miro888 <120926830+miro888@users.noreply.github.com>
2023-05-13 23:23:42 +0400

    create react app / scss installed

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './scss/App.scss';
+// import { useEffect } from 'react';
+
+
+
+
+function App() {
+ 
+
+  return (
+    <div className="app">
+     <div>Left</div>
+     <div>middl</div>
+     <div>right</div>
+    </div>
+  );
+}
+
+
+export default App;
